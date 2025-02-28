186900e2f67cc4ddba393fe61e8681ab63869ff2
levomaaa <148433955+levomaaa@users.noreply.github.com>
2025-02-28 15:03:55 +0200

    Creating flaw-2

--- pages/views.py
+++ pages/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 User = get_user_model()
 
+# FIX: CSRF --> remove the @csrf_exempt below
 @csrf_exempt
 def login_view(request):  
     if request.method == 'POST':  
@@ -25,17 +26,23 @@ def logout_view(request):
     logout(request)  
     return redirect('/')  
 
-@csrf_exempt
-#@login_required  
+# FIX: CSRF --> remove the @csrf_exempt below
+@csrf_exempt  
 def profile_view(request, username): 
+    # FIX: Broken Access Control --> take the lines below out of comments
+    #if not request.user.is_authenticated:
+    #    return redirect('/')
+    #if request.user.username != username:  
+    #    return redirect('profile', username=request.user.username)
 
-    user = get_object_or_404(User, username=username) 
+    user = get_object_or_404(User, username=username)
+
     if request.method == 'POST':  
         form = ProfileUpdateForm(request.POST, instance=user)  
         if form.is_valid():  
             form.save()  
-            return redirect('profile', username=user.username) 
+            return redirect('profile', username=user.username)  
     else:  
         form = ProfileUpdateForm(instance=user)  
 
-    return render(request, 'profile.html', {'form': form, 'user': user}) 
+    return render(request, 'profile.html', {'form': form, 'user': user})  
